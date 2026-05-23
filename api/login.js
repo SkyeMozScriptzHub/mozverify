7ccf23ecb71100974f9056a41650f26fa9cfb3df
SkyeMozYT <SkyeMozOfficial@gmail.com>
2026-05-23 13:37:29 +0800

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const REDIRECT_URI = process.env.REDIRECT_URI;
+
+  const url =
+`https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=identify%20guilds.join`;
+
+  res.redirect(url);
+}
